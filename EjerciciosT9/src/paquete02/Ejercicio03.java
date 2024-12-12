f7304a34550afc5ea4fd0d4148c759b2bcf950a9
jhandry naula <jjnaula3@utpl,edu,ec>
2024-12-12 12:43:00 -0500

    cambios4

--- EjerciciosT9/src/paquete02/Ejercicio03.java
+++ EjerciciosT9/src/paquete02/Ejercicio03.java
@@ -17,7 +17,28 @@ public class Ejercicio03 {
     public static void main(String[] args) {
         // TODO code application logic here
         int[] arreglo = {1, 10, 11, 12, 12, 13, 16, 2, 3, 4, 9, 10, 21};
-        
+        double suma = 0;
+        String arriba = "";
+        String abajo = "";
+
+        for (int i = 0; i < arreglo.length; i++) {
+            suma = suma + arreglo[i];
+
+        }
+        double media = suma / arreglo.length;
+
+        for (int i = 0; i < arreglo.length; i++) {
+            if (arreglo[i] < media) {
+                arriba = String.format("%s %d ",arriba,arreglo[i]);
+
+            } else {
+                abajo = String.format("%s %d ",abajo,arreglo[i]);
+            }
+        }
+        System.out.printf("La media aritmetica es: %.2f\nLos elementos por "
+                + "arriba son: %s\nLos elementos por debajo son: %s\n", media,
+                arriba,
+                abajo);
     }
 
 }
