2a9f0cacbf2b5d0481444af2bf6680b3b478adb0
jhandry naula <jjnaula3@utpl,edu,ec>
2024-12-12 12:04:44 -0500

    cambios11

--- EjerciciosT9/src/paquete01/Ejercicio.java
+++ EjerciciosT9/src/paquete01/Ejercicio.java
@@ -4,10 +4,48 @@
  */
 package paquete01;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
 public class Ejercicio {
-    
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int limite;
+        String dato;
+        String mensajeFinal = "Los datos ingresados son:\n";
+        String letra2;
+        System.out.println("Ingrese el numero de elementos que desea:");
+        limite = entrada.nextInt();
+        entrada.nextLine();
+
+        String[] arreglo = new String[limite];
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese la marca");
+            dato = entrada.nextLine();
+            arreglo[i] = dato;
+            dato = dato.toUpperCase();
+            char letra = dato.charAt(0);
+          
+        
+           
+            letra2 = String.valueOf(letra);
+
+            if (letra2.equals("A") || letra2.equals("C") || letra2.equals("T")) {
+               
+            } else {
+                mensajeFinal = String.format("%s%s\n", mensajeFinal,
+                        arreglo[i]);
+
+            }
+        }
+        System.out.printf("%s", mensajeFinal);
+    }
 }
