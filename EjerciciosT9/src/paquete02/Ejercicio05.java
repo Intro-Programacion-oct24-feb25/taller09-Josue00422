558b29cd9324c6618e78fc0eb04d1bd4f0146ef6
jhandry naula <jjnaula3@utpl,edu,ec>
2024-12-13 08:34:32 -0500

    cambios 60

--- EjerciciosT9/src/paquete02/Ejercicio05.java
+++ EjerciciosT9/src/paquete02/Ejercicio05.java
@@ -17,9 +17,21 @@ public class Ejercicio05 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-      
+        Scanner entrada = new Scanner(System.in);
+        double[] Ventas = new double[5];
+        String[] Dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes"};
+
+        for (int i = 0; i < Dias.length; i++) {
+            System.out.printf("Ingresar el valor de ventas del dia %s\n",
+                    Dias[i]);
+
+            double venta = entrada.nextDouble();
+            Ventas[i] = venta;
+
+        }
+        for (int i = 0; i < Dias.length; i++) {
+            System.out.printf("· %s $%.0f\n", Dias[i], Ventas[i]);
+        }
 
     }
-
 }
